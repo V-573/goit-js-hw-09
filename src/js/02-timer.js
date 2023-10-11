057497549faada8238fe28e4499364ae8dc8cbac
vaj573 <jhonedw573@gmail.com>
2023-10-11 01:02:02 -0500

    tarea 2 casi finalizada. aun faltan detalles

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,35 +1,82 @@
 import flatpickr from "flatpickr";
 import "/node_modules/flatpickr/dist/flatpickr.min.css";
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
+let timerInterval; // Variable para almacenar el intervalo del temporizador
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+document.addEventListener('DOMContentLoaded', function () {
+  
+  const btnStart = document.querySelector('[data-start]');
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const options = {
+    enableTime: true,
+    time_24hr: true,
+    minuteIncrement: 1,
 
-  return { days, hours, minutes, seconds };
-}
+    onClose(selectedDates) {
+      const targetDate = selectedDates[0];
+      updateTimer(targetDate);
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+      // Actualiza el temporizador cada segundo
+      timerInterval = setInterval(() => {
+        updateTimer(targetDate);
+      }, 1000);
+    },
+
+
+  };
+
+
+
+  flatpickr("#datetime-picker", options);
+
+  function updateTimer(targetDate) {
+    const currentDate = new Date();
+
+    // Calcula la diferencia en milisegundos
+    const ms = targetDate.getTime() - currentDate.getTime();
+
+    // Si la fecha ya ha pasado, detén el temporizador
+    if (ms <= 0) {
+      
+    
+      clearInterval(timerInterval);
+      alert("¡Tiempo finalizado!");
+      btnStart.disabled = true;
+      return;
+    }
+
+    // Convierte la diferencia a días, horas, minutos y segundos
+
+    btnStart.disabled = false;
+    
+      
+   
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+
+
+    // Actualiza el HTML con los valores calculados
+    requestAnimationFrame(() => {
+      document.querySelector('[data-days]').textContent = formatTimeUnit(days);
+      document.querySelector('[data-hours]').textContent = formatTimeUnit(hours);
+      document.querySelector('[data-minutes]').textContent = formatTimeUnit(minutes);
+      document.querySelector('[data-seconds]').textContent = formatTimeUnit(seconds);
+    });
+  
+
+  }
+
+  function formatTimeUnit(unit) {
+    return unit < 10 ? `0${unit}` : unit;
+  }
+});
