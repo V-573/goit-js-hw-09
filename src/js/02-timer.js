431b314545656669a64e6db6d68533b519dd61d8
vaj573 <jhonedw573@gmail.com>
2023-10-14 20:59:19 -0500

    arreglo del punto 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -23,52 +23,62 @@ document.addEventListener('DOMContentLoaded', function () {
         updateTimer(targetDate);
       }, 1000);
     },
-  };
+     onChange(selectedDates) {
+      if (selectedDates.length > 0) {
+        // Si se ha seleccionado una fecha, habilita el botón
+        btnStart.disabled = false;
+      } else {
+        // Si no se ha seleccionado ninguna fecha, deshabilita el botón
+        btnStart.disabled = true;
+      }
+    },
+  };// hasta aqui lo correspondiente a option 
+
 
   flatpickr("#datetime-picker", options);
 
   function updateTimer(targetDate) {
     const currentDate = new Date();
 
-    // Calcula la diferencia en milisegundos
+      // Calcula la diferencia en milisegundos
     const ms = targetDate.getTime() - currentDate.getTime();
 
-    // Si la fecha ya ha pasado, detén el temporizador
+      // Si la fecha ya ha pasado, detén el temporizador
     if (ms <= 0) {
-        clearInterval(timerInterval);
-          Notiflix.Notify.info('¡please choose in the future time!');
-        btnStart.disabled = true;
-      return;
-    }
+          clearInterval(timerInterval);
+            Notiflix.Notify.info('¡please choose in the future time!');
+          btnStart.disabled = true;
+        return;
+      }
 
-    // Convierte la diferencia a días, horas, minutos y segundos
+      // Convierte la diferencia a días, horas, minutos y segundos
 
     btnStart.disabled = false;
-    
       
-   
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+        
+    
+      const second = 1000;
+      const minute = second * 60;
+      const hour = minute * 60;
+      const day = hour * 24;
+      const days = Math.floor(ms / day);
+      // Remaining hours
+      const hours = Math.floor((ms % day) / hour);
+      // Remaining minutes
+      const minutes = Math.floor(((ms % day) % hour) / minute);
+      // Remaining seconds
+      const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-
-
-    // Actualiza el HTML con los valores calculados
+      // Actualiza el HTML con los valores calculados
+      
+        document.querySelector('[data-days]').textContent = formatTimeUnit(days);
+        document.querySelector('[data-hours]').textContent = formatTimeUnit(hours);
+        document.querySelector('[data-minutes]').textContent = formatTimeUnit(minutes);
+        document.querySelector('[data-seconds]').textContent = formatTimeUnit(seconds);
+    
     requestAnimationFrame(() => {
-      document.querySelector('[data-days]').textContent = formatTimeUnit(days);
-      document.querySelector('[data-hours]').textContent = formatTimeUnit(hours);
-      document.querySelector('[data-minutes]').textContent = formatTimeUnit(minutes);
-      document.querySelector('[data-seconds]').textContent = formatTimeUnit(seconds);
-    });
-  
+      });
+    
 
   }
 
