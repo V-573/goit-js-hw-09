d94c62f51c67183e60177023d3e51a13d2404aac
vaj573 <jhonedw573@gmail.com>
2023-10-14 21:11:31 -0500

    comento el js del punto 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,15 +1,14 @@
-import flatpickr from "flatpickr";
+import flatpickr from "flatpickr";// importo la biblioteca y su estilo
 import "/node_modules/flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
-
+// primero: configuracion de flatpickr y eventos:
 let timerInterval; // Variable para almacenar el intervalo del temporizador
 
-document.addEventListener('DOMContentLoaded', function () {
-  
+document.addEventListener('DOMContentLoaded', function () {// este evento se dispara cuando el html ha sido cargado
   const btnStart = document.querySelector('[data-start]');
 
-  const options = {
+  const options = {// configuracion de flatpickr
     enableTime: true,
     time_24hr: true,
     minuteIncrement: 1,
@@ -23,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
         updateTimer(targetDate);
       }, 1000);
     },
-     onChange(selectedDates) {
+    onChange(selectedDates) {
       if (selectedDates.length > 0) {
         // Si se ha seleccionado una fecha, habilita el botón
         btnStart.disabled = false;
@@ -35,8 +34,9 @@ document.addEventListener('DOMContentLoaded', function () {
   };// hasta aqui lo correspondiente a option 
 
 
-  flatpickr("#datetime-picker", options);
+  flatpickr("#datetime-picker", options); // parte 2 configura Flatpickr en el elemento con el ID "datetime-picker" utilizando las opciones definidas anteriormente
 
+  // Parte 3: funcion updateTimer
   function updateTimer(targetDate) {
     const currentDate = new Date();
 
@@ -49,37 +49,31 @@ document.addEventListener('DOMContentLoaded', function () {
             Notiflix.Notify.info('¡please choose in the future time!');
           btnStart.disabled = true;
         return;
-      }
+    }
 
       // Convierte la diferencia a días, horas, minutos y segundos
 
     btnStart.disabled = false;
-      
-        
-    
-      const second = 1000;
-      const minute = second * 60;
-      const hour = minute * 60;
-      const day = hour * 24;
-      const days = Math.floor(ms / day);
-      // Remaining hours
-      const hours = Math.floor((ms % day) / hour);
-      // Remaining minutes
-      const minutes = Math.floor(((ms % day) % hour) / minute);
-      // Remaining seconds
-      const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-      // Actualiza el HTML con los valores calculados
-      
-        document.querySelector('[data-days]').textContent = formatTimeUnit(days);
-        document.querySelector('[data-hours]').textContent = formatTimeUnit(hours);
-        document.querySelector('[data-minutes]').textContent = formatTimeUnit(minutes);
-        document.querySelector('[data-seconds]').textContent = formatTimeUnit(seconds);
-    
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    // Actualiza el HTML con los valores calculados
+  
+    document.querySelector('[data-days]').textContent = formatTimeUnit(days);
+    document.querySelector('[data-hours]').textContent = formatTimeUnit(hours);
+    document.querySelector('[data-minutes]').textContent = formatTimeUnit(minutes);
+    document.querySelector('[data-seconds]').textContent = formatTimeUnit(seconds);
+  
     requestAnimationFrame(() => {
-      });
-    
-
+    });
   }
 
   function formatTimeUnit(unit) {
